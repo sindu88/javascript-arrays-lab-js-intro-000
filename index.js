701579cde64c7f49e3f828d8e64e2fc960cd3007
sinduja <sindhu.38@gmail.com>
2019-05-16 02:46:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-var kittens = [] //define your array here
-
+var kittens = ["Milo","Otis","Garfield"] ;//define your array here
+function destructivelyAppendKitten(name)
+{
+  kittens.push(name);
+}
 // Add your functions and code here
+function destructivelyPrependKitten(name)
+{
+  kittens.shift(name);
+}
+function destructivelyRemoveLastKitten()
+{
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten()
+{
+kittens.unshift();
+}
+function appendKitten(name)
+{
+var newArray= kittens.concat(name);
+return newArray;
+
+}
+function prependKitten(name)
+{
+
+}
+
+
+function RemoveLastKitten()
+{
+}
+
+function RemoveFirstKitten()
+{
+  var newArray= kittens.slice(1);
+  return newArray;
+}
